165e9ef3f3237538a2593f1390094bc0d69f2436
choi su ho <csw020106@naver.com>
2021-11-11 16:38:51 +0900

    1-3단계입니다(고양이)

---
+++ 백준알고리즘1단계/Algorithm1-3.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(void){
+    printf("\\");
+    printf("    /");
+    printf("\\");
+    printf("\n");
+    printf(" )");
+    printf("  ( \')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|");
+    
+    return 0;
+}
