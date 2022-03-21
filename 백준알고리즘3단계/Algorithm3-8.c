09a7ef654dd3126b0f335d03cf8210ff5f8d469d
choi su ho <csw020106@naver.com>
2022-03-21 17:25:48 +0900

    A + B -8 8단계

--- 백준알고리즘3단계/Algorithm3-8.c
+++ 백준알고리즘3단계/Algorithm3-8.c
@@ -4,12 +4,21 @@
 
 int main(void)
 {
-    int  T = 0, A  = 0, B = 0;    
-    scanf("%d",&T);
-    for(int i = 1; i <= T; i++)
-    {       
-        scanf("%d %d",&A,&B);
-        printf("Case #%d: %d + %d = %d\n",i,A,B,A+B);
+   // 规过 1
+    int t,a,b;
+    scanf("%d",&t);
+    for(int i = 1; i <= t; i++)
+    {
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+    }
+    // 规过 2
+    int i = 1;
+    while (i <= t)
+    {
+        scanf("%d %d",&a,&b);
+        printf("Case #%d: %d + %d = %d\n",i,a,b,a+b);
+        i++;
     }
 
     return 0;
