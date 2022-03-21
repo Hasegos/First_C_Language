58ae29011325d123f30419f5b917e09477419479
choi su ho <csw020106@naver.com>
2022-03-21 17:26:52 +0900

    X보다 작은 수 11단계

--- 백준알고리즘3단계/Algorithm3-11.c
+++ 백준알고리즘3단계/Algorithm3-11.c
@@ -5,18 +5,31 @@
 
 int main(void)
 {
-    int X = 0 , N = 0;    
-    scanf("%d %d",&N,&X);
-    int a[10000] ={0};   
-    for(int i=0; i<N; i++)
+    // 规过 1
+    int x, n, A[10000] = { 0, };
+    scanf("%d %d", &n, &x);
+    for (int i = 1; i <= n; i++)
     {
-        scanf("%d",a[i]);
-        if(X > a[i])
-        {
-            printf("%d\n",a[i]);
-        }       
+        scanf("%d", &A[i]);
     }
-
+    for (int i = 1; i <= n; i++)
+    {
+        if (x > A[i])
+        {
+            printf("%d ", A[i]);
+        }
+    }
+    // 规过 2
+    int a;
+    for (int i = 1; i <= n; i++)
+    {
+        scanf("%d", &a);
+        if (x > a)
+        {
+            printf("%d ", a);
+        }
+    }
+    
     return 0;
 }
 
