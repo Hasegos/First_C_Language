07bd2a9bee81dbdfbe2353d7253618fc8395dde8
choi su ho <csw020106@naver.com>
2022-03-21 17:26:14 +0900

    별 찍기 -1 9단계

--- 백준알고리즘3단계/Algorithm3-9.c
+++ 백준알고리즘3단계/Algorithm3-9.c
@@ -4,15 +4,29 @@
 
 int main(void)
 {
-    int N = 0;    
+     // 规过 1
+    int N;
     scanf("%d",&N);
     for(int i = 1; i <= N; i++)
     {
-        for(int a = 1; a <=i; a++)
+        for(int j = 1; j <= i; j++)
         {
             printf("*");
         }
         printf("\n");
+    }
+    // 规过 2
+    int i = 1;
+    while (i <= N)
+    {
+        int j = 1;
+        while (j <= i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i ++;       
     }
     
     return 0;
