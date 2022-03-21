0b340719b8b631338e992741cb611c4e57328d50
choi su ho <csw020106@naver.com>
2022-03-21 17:26:32 +0900

    별 찍기 -2 10단계

--- 백준알고리즘3단계/Algorithm3-10.c
+++ 백준알고리즘3단계/Algorithm3-10.c
@@ -5,19 +5,39 @@
 
 int main(void)
 {
-    int N = 0;
-    scanf("%d",&N); // 입력받기
-    for(int i = 1; i <= N; i++) // 횟수
+    // 방법 1
+    int N,l;
+    scanf("%d",&N);
+    for(int i = 1; i <= N; i++)
     {
-        for(int j = N; j > i; j--) // 빈칸을 만들위한 반복 
+        for(int k = N; k > i; k--)
         {
             printf(" ");
         }
-        for(int a = 1; a <= i; a++) //  별 찍기
+        for(int j = 1; j <= i; j++)
         {
             printf("*");
         }
-        printf("\n"); // 다 하고 나서 줄바꿈
+        printf("\n");
+    }
+    // 방법 2
+    int i = 1;
+    while (i <= N)
+    {
+        l = N;
+        while ( l > i)
+        {
+            printf(" ");
+            l--;
+        }        
+        int j = 1;
+        while (j <= i)
+        {
+            printf("*");
+            j++;
+        }
+        printf("\n");
+        i ++;       
     }
     
     return 0;
