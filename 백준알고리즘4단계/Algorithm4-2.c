0ac37a95e0b12cddd6ec7fc082a908b2aef95237
choi su ho <csw020106@naver.com>
2022-03-21 17:39:28 +0900

    A + B -4 2단계

--- 백준알고리즘4단계/Algorithm4-2.c
+++ 백준알고리즘4단계/Algorithm4-2.c
@@ -6,13 +6,12 @@
 
 int main(void)
 {
-     // 방법 1
+    // 방법 1
     int a = 1, b = 1;
     while (scanf("%d %d", &a, &b) != EOF)  // EOF : End Of File 이라는 약자로 텍스트 파일의 끝임 
     {                                      // 즉, 우리가 무한대로 빠진걸 종료시킬때 ctrl + z누르는 값(-1)이랑 같음!
         printf("%d\n", a + b);             // 기억하기 
     }  
-    return 0;
-    
-    return 0;
+    return 0;   
+ 
 }
