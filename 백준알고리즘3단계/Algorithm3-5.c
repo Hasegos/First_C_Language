bff4af40507d8cb8ea3a0e6ab73aee4f51310e89
choi su ho <csw020106@naver.com>
2022-03-21 17:13:49 +0900

    N 찍기 5단계

--- 백준알고리즘3단계/Algorithm3-5.c
+++ 백준알고리즘3단계/Algorithm3-5.c
@@ -1,12 +1,11 @@
 #include<stdio.h>
+
 //자연수 N이 주어졌을 때, 1부터 N까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
 
 int main(void)
 {
-    int N = 0;
-    printf("자연수 N 값을 입력하세요.");
+    int N = 0;  
     scanf("%d",&N);
-
     for(int i = 1; i  <= N; i++)
     {
         printf("%d\n",i);
