d45f6497bc0f635ddb0c1076ddd3df767fa88cd7
choi su ho <csw020106@naver.com>
2022-03-21 17:36:53 +0900

    A + B -5 1단계

--- 백준알고리즘4단계/Algorithm4-1.c
+++ 백준알고리즘4단계/Algorithm4-1.c
@@ -1,18 +1,31 @@
 #include<stdio.h>
+
 //두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
 int main(void)
 {
-    int A = 0, B = 0; // 0 으로 초기화
-    while (1) // 무한 반복
+   // 방법 1
+    int a, b;
+    while (1)
     {
-        scanf("%d %d",&A,&B); // 두 정수 입력받기
-        if(A == 0 && B == 0) // 둘다 0이면 종료
+        scanf("%d %d", &a, &b);        
+        if (a == 0 && b == 0)
         {
-            break; // 종료
+            break;
         }
-        printf("%d\n",A+B); // 더한거 출력
-        
+        printf("%d\n", a + b);
     }
+    // 방법 2
+    for(int i = 0;; i++)
+    {
+        scanf("%d %d", &a, &b);        
+        if (a == 0 && b == 0)
+        {
+            break;
+        }
+        printf("%d\n", a + b);
+    }
+
     
 
     return 0;
