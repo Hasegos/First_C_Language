02b93caad93d524138dcc70c3bc4d6800d5f1b5d
choi su ho <csw020106@naver.com>
2022-03-21 17:14:08 +0900

    기찍 N 6단계

--- 백준알고리즘3단계/Algorithm3-6.c
+++ 백준알고리즘3단계/Algorithm3-6.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
+
 //자연수 N이 주어졌을 때, N부터 1까지 한 줄에 하나씩 출력하는 프로그램을 작성하시오.
+
 int main(void)
 {
-    int N = 0;
-    printf("자연수 N을 입력하세요.");
+    int N = 0;    
     scanf("%d",&N);
-
     for(int i = N; i > 0; i--)
     {
         printf("%d\n",i);
     }
-
-
+    
     return 0;
 }
