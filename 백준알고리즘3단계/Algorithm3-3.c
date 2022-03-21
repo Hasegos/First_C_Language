58dac5557fb2c3b610efb219329bb8979701309a
choi su ho <csw020106@naver.com>
2022-03-21 17:13:00 +0900

    합 3단계

--- 백준알고리즘3단계/Algorithm3-3.c
+++ 백준알고리즘3단계/Algorithm3-3.c
@@ -1,18 +1,16 @@
 #include<stdio.h>
+
 //n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
 
 int main(void)
 {
-    int N=0,k=0; // 입력 받을 N 과 누적할 K 변수 설정
-    printf("원하는 자연수를 입력하세요.");
+    int N=0,k=0; // 입력 받을 N 과 누적할 K 변수 설정    
     scanf("%d",&N);
-
     for(int i=1; i<=N; i++) // 1부터 입력받은 수까지 계속 반복
     {
         k+=i; // 해당 수 누적
     }
     printf("%d",k);
-
 
     return 0;
 }
