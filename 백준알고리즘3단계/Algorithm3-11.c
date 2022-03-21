fa5038cc7ac51484c52e23192efa31c9565b9c8b
choi su ho <csw020106@naver.com>
2022-03-21 17:15:51 +0900

    X보다 작은 수 11단계

--- 백준알고리즘3단계/Algorithm3-11.c
+++ 백준알고리즘3단계/Algorithm3-11.c
@@ -1,9 +1,11 @@
 #include<stdio.h>
-//정수 N개로 이루어진 수열 A와 정수 X가 주어진다. 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
+// 정수 N개로 이루어진 수열 A와 정수 X가 주어진다.
+// 이때, A에서 X보다 작은 수를 모두 출력하는 프로그램을 작성하시오.
+
 int main(void)
 {
-    int X = 0 , N = 0;
-    printf("두 정수 X 와 N을 입력하세요");
+    int X = 0 , N = 0;    
     scanf("%d %d",&N,&X);
     int a[10000] ={0};   
     for(int i=0; i<N; i++)
@@ -12,8 +14,7 @@ int main(void)
         if(X > a[i])
         {
             printf("%d\n",a[i]);
-        }
-       
+        }       
     }
 
     return 0;
